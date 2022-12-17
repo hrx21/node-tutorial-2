62a3f63b9091ecb67065e50c3a2a2e459a2668d9
Ninad Muranjan <hrxprog69@gmail.com>
2022-12-17 18:28:21 +0530

    doiehw7tdfctw

---
+++ global.js
@@ -0,0 +1,16 @@
+// console.log(global)
+
+// setTimeout(() => {
+//     console.log('I will stop you')
+//     clearInterval(int)
+// }, 5000);
+
+// const int = setInterval(() => {
+//     console.log('I will keep running')
+// }, 1000)
+
+
+// console.log(__dirname)
+// console.log(__filename)
+
+console.log(document.querySelector)  // this is window object won't work in global object. 
---
+++ people.js
@@ -0,0 +1,9 @@
+const people = ['james', 'jessie', 'ash', 'brock', 'misty']
+const age = [20, 19, 12, 14, 12]
+
+console.log(people)
+
+
+module.exports = {
+    people, age
+}
